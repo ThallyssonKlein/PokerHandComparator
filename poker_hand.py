82679d1c7ce4adedb91d459fe3fe32b7678f6449
thallyssonklein <thallyssonklein@gmail.com>
2021-04-15 04:25:00 -0300

    isFullHouse done

--- poker_hand.py
+++ poker_hand.py
@@ -1,6 +1,7 @@
 from card import Card
 from enum import Enum
 
+
 class PokerHand():
     __cards = []
     handTypeNumber = 0
@@ -8,23 +9,6 @@ class PokerHand():
 
     def getCards(self):
         return self.__cards
-
-    def __init__(self, cardsStr):
-        cards = cardsStr.split(' ')
-
-        if len(cards) != 5:
-            raise Exception('Invalid number of cards')
-
-        for card in cards:
-            self.__cards.append(Card(value=card))
-
-        defineHand(self)
-
-    def defineHand(self, hand):
-        if isRoyalFlush(hand):
-            hand.handTypeNumber = 10
-        elif isStraightFlush(hand):
-            hand.handTypeNumber = 9
 
     def isRoyalFlush(self, hand):
         cards = hand.getCards()
@@ -71,14 +55,52 @@ class PokerHand():
                 if iCount == 4:
                     return True
         return False
+
     def isTwoPair(self):
         pass
 
-    def isFullHouse(self):
-        pass
+    def isFullHouse(self, hand):
+        cards = hand.getCards()
+        hasThree = False
+        for i in range(0, len(cards)):
+            if not hasThree:
+                iCount = 1
+                for j in range(i + 1, len(cards)):
+                    if cards[i].cardValue == cards[j].cardValue:
+                        iCount += 1
+                    if iCount == 3:
+                        hasThree = True
+                        iCount = 0
+            else:
+                iCount = 1
+                for j in range(i + 1, len(cards)):
+                    if cards[i].cardValue == cards[j].cardValue:
+                        iCount += 1
+                    if iCount == 2:
+                        return True
+        return False
 
     def isOnePair(self):
         pass
+
+    def defineHand(self, hand):
+        if isRoyalFlush(hand):
+            hand.handTypeNumber = 10
+        elif isStraightFlush(hand):
+            hand.handTypeNumber = 9
+        elif isFullHouse(hand):
+            hand.handTypeNumber = 8
+
+    def __init__(self, cardsStr):
+        cards = cardsStr.split(' ')
+
+        if len(cards) != 5:
+            raise Exception('Invalid number of cards')
+
+        for card in cards:
+            self.__cards.append(Card(value=card))
+
+        defineHand(self)
 
     def compare_with(self, other):
         pass
